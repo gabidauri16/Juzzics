7c78528907c07d8b56b55f0df691b667d78087be
ggabidauri <ggabidauri@tbcbank.com.ge>
2024-03-26 11:32:12 +0400

    shortened vm functions a little

--- app/src/main/java/com/example/juzzics/features/musics/ui/vm/logics/OnDragEndLogic.kt
+++ app/src/main/java/com/example/juzzics/features/musics/ui/vm/logics/OnDragEndLogic.kt
@@ -6,6 +6,4 @@ import com.example.juzzics.features.musics.ui.vm.MusicVM
 import com.example.juzzics.features.musics.ui.vm.MusicVM.Companion.MUSIC_LIST
 import kotlinx.collections.immutable.toImmutableList
 
-fun MusicVM.onDragEnd(list: SnapshotStateList<MusicFileUi>) {
-    MUSIC_LIST(list.toImmutableList())
-}
+fun MusicVM.onDragEnd(list: SnapshotStateList<MusicFileUi>) = MUSIC_LIST(list.toImmutableList())
--- app/src/main/java/com/example/juzzics/features/musics/ui/vm/logics/SeekToLogic.kt
+++ app/src/main/java/com/example/juzzics/features/musics/ui/vm/logics/SeekToLogic.kt
@@ -5,6 +5,7 @@ import com.example.juzzics.features.musics.ui.vm.MusicVM
 import com.example.juzzics.features.musics.ui.vm.MusicVM.Companion.MEDIA_PLAYER
 
 fun MusicVM.seekTo(position: Float) {
-    val mediaPlayer = MEDIA_PLAYER<MediaPlayer>()
-    mediaPlayer?.seekTo((position * mediaPlayer.duration.toFloat()).toInt())
+    MEDIA_PLAYER<MediaPlayer>()?.apply {
+        seekTo((position * duration.toFloat()).toInt())
+    }
 }
