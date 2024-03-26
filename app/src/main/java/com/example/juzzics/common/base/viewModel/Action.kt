dcc3d0697f711ae8dddc0bd7a91f32d78dd106a3
ggabidauri <ggabidauri@tbcbank.com.ge>
2024-03-26 11:18:33 +0400

    added little comments

--- app/src/main/java/com/example/juzzics/common/base/viewModel/Action.kt
+++ app/src/main/java/com/example/juzzics/common/base/viewModel/Action.kt
@@ -1,3 +1,4 @@
 package com.example.juzzics.common.base.viewModel
 
+/** use for sending actions from Screen to ViewModel */
 interface Action
--- app/src/main/java/com/example/juzzics/common/base/viewModel/UiEvent.kt
+++ app/src/main/java/com/example/juzzics/common/base/viewModel/UiEvent.kt
@@ -1,5 +1,6 @@
 package com.example.juzzics.common.base.viewModel
 
+/** use for sending events from ViewModel to the Screen */
 interface UiEvent {
     data class Message(val msg: String) : UiEvent
     data class Loading(val loading: Boolean = true) : UiEvent
